4da139c4552f46ac5c31fb672dd5f8b88c2a77b8
Ajay Ramesh <carpetfizz@gmail.com>
2014-05-04 18:11:04 -0700

    Fixed albums bug

--- jimgur.py
+++ jimgur.py
@@ -1,4 +1,5 @@
 import praw,urllib2,json,time
+from pprint import pprint
 
 r = praw.Reddit("Jimgur image linker by /u/carpetfizz")
 r.login('username','password')
@@ -10,24 +11,23 @@ while True:
 		if submission.id not in already_done:
 			links_list = []
 			request = None
-			if url.startswith("http://imgur.com/"):
-				image_id = url.rsplit('/',1)[1]
-				request = urllib2.Request("https://api.imgur.com/3/image/%s"%image_id)
-				request.isimage = True
-			elif url.startswith("http://imgur.com/a/"):
+			if url.startswith("http://imgur.com/a/"):
 				album_id = url.rsplit('/',1)[1]
 				request = urllib2.Request("https://api.imgur.com/3/album/%s/images"%album_id)
 				request.isimage = False
+			elif url.startswith("http://imgur.com/"):
+				image_id = url.rsplit('/',1)[1]
+				request = urllib2.Request("https://api.imgur.com/3/image/%s"%image_id)
+				request.isimage = True
 			already_done.append(submission.id)
 			if request:
-				#Register as an imgur app here: https://api.imgur.com/
 				request.add_header("Authorization","Client-ID "+"YOUR_CLIENT_ID")
 				try:
 					response = json.loads(urllib2.urlopen(request).read())
 					if request.isimage:
 						links_list.append(response[u'data'][u'link'])
 					else:
-						for image in response[u'data'][u'images']:
+						for image in response[u'data']:
 							links_list.append(image[u'link'])
 				except urllib2.HTTPError:
 					continue
